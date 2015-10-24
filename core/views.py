3ff23466ddac96708b4d0ca121199b49519951bb
vishesh <visheshbangotra@gmail.com>
2015-10-25 04:38:54 +0530

    init material design

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render_to_response
 
 
 def home(request):
-    return HttpResponse("WELCOME TO AETOS KODIKA")
+    # return HttpResponse("WELCOME TO AETOS KODIKA")
+    return render_to_response("core/home.html", {})
