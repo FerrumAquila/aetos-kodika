bdcee98cbe7fc7e699f487fe6745426a50d9765b
vishesh <visheshbangotra@gmail.com>
2015-10-25 16:29:52 +0530

    added home and index views

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ import views as app_views
 
 urlpatterns = [
     url(r'^profile/$', app_views.profile, name='core_profile'),
+    url(r'^home/$', app_views.home, name='core_home'),
+    url(r'^$', app_views.index, name='core_index'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import HttpResponse, render_to_response
 
 
+def index(request):
+    return render_to_response("core/home.html", {})
+
+
+def home(request):
+    return render_to_response("core/home.html", {})
+
+
 def profile(request):
     return render_to_response("core/profile.html", {})
