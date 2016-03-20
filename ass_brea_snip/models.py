90f6347f8be3bc1765d43823ab60ffe48a8c8319
vishesh <visheshbangotra@gmail.com>
2016-03-20 15:49:19 +0530

    added tracking field

--- ass_brea_snip/models.py
+++ ass_brea_snip/models.py
@@ -1,3 +1,18 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+# from IPython.frontend.terminal.embed import InteractiveShellEmbed
+# InteractiveShellEmbed()()
+
+
+class TrackingFields(models.Model):
+    """
+    Each model will have these fields
+    """
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
