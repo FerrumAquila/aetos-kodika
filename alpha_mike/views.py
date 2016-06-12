4abb0c5596e566bb18967da433b2b97480058541
vishesh <visheshbangotra@gmail.com>
2016-06-13 00:45:49 +0530

    added logging and content

--- alpha_mike/views.py
+++ alpha_mike/views.py
@@ -1,9 +1,15 @@
 __author__ = 'ironeagle'
 
+import datetime
 
 from django.shortcuts import *
 
 
 def index(request):
+    with open('am_access.log', 'w+') as f:
+        f.write(str(request))
+        f.write('\n')
+        f.write(str(datetime.datetime.now()))
+        f.write('\n')
     return render(request, "am-index.html", {})
 
