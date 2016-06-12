fb8b0cd165db5850142e0aebf11f6d5c0625df7b
vishesh <visheshbangotra@gmail.com>
2016-06-13 00:55:27 +0530

    improved logging

--- alpha_mike/views.py
+++ alpha_mike/views.py
@@ -6,10 +6,12 @@ from django.shortcuts import *
 
 
 def index(request):
+    # from IPython.frontend.terminal.embed import InteractiveShellEmbed
+    # InteractiveShellEmbed()()
     with open('am_access.log', 'w+') as f:
-        f.write(str(request))
-        f.write('\n')
+        f.write(str(request.__dict__))
+        f.write('\n================================================================')
         f.write(str(datetime.datetime.now()))
-        f.write('\n')
+        f.write('\n================================================================')
     return render(request, "am-index.html", {})
 
